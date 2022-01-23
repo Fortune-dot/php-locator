20a79f04666f97d4d7849175a3e1d730d1599763
Fortune <fortunelangat54@gmail.com>
2022-01-23 23:17:46 +0300

    api-int

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+// create curl resource
+$ch = curl_init();
+
+// set url
+curl_setopt($ch, CURLOPT_URL, "https://api.ipgeolocation.io/ipgeo?apiKey=4b5a37787c784cf19cc89ea7834fefa7&ip=".$ip);
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string
+$output = curl_exec($ch);
+print_r($output);
+
+// close curl resource to free up system resources
+curl_close($ch);     
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User location using php</title>
+</head>
+<body>
+    
+</body>
+</html>
